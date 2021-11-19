6e0a4b6d62580ba6ee57f11c257b284592081373
MattDevman <mateusxxt120@gmail.com>
2021-11-19 01:58:54 -0300

    Player init

---
+++ player.py
@@ -0,0 +1,15 @@
+class Avatar:
+    nick = ""
+    vida = 0
+    lvl = 0
+    classe = "Humano"
+
+
+class Avatar_config(Avatar):
+    def setNick(self):
+        a_nick = str(input("Digite o nome do personagem: "))
+        self.nick = a_nick
+        self.vida = 100
+
+    setNick(Avatar)
+
